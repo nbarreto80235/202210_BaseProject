ea933f02924ea59bcaa4f48f56364084b1fb6577
nbarreto80235 <nbarreto@uniandes.edu.co>
2022-04-29 18:33:07 -0500

    Punto 2 del parcial

--- src/app/plantas/plantas-list/plantas-list.component.ts
+++ src/app/plantas/plantas-list/plantas-list.component.ts
@@ -10,12 +10,17 @@ import { PlantaService } from '../planta.service';
 export class PlantasListComponent implements OnInit {
 
   plantas: Array<Planta> = [];
+  sumInterior: number = 0;
+  sumExterior: number = 0;
 
   constructor(private plantasSevice: PlantaService) { }
 
   getPlantas(): void {
     this.plantasSevice.getPlantas().subscribe((plantas) => {
       this.plantas = plantas;
+      this.sumExterior = this.plantas.filter(p => p.tipo === 'Exterior').length;
+      this.sumInterior = this.plantas.filter(p => p.tipo === 'Interior').length;
+
     });
   }
 
